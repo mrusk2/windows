bce9c249d1d298169e13c79cf5c9a8c7bb5739f6
mrusk2 <mason.m.rusk@gmail.com>
2025-06-09 15:49:56 -0400

    Initial Commit

---
+++ windows/windowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  windowsApp.swift
+//  windows
+//
+//  Created by Mason Rusk on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct windowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ windowsTests/windowsTests.swift
@@ -0,0 +1,17 @@
+//
+//  windowsTests.swift
+//  windowsTests
+//
+//  Created by Mason Rusk on 6/9/25.
+//
+
+import Testing
+@testable import windows
+
+struct windowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
